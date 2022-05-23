79fae669a8e550a904a16debea35a3c085d66871
TalentDev1128 <razorking1128@gmail.com>
2022-05-22 20:34:45 -0400

    make not auto playable on how-to vieos

--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -1,85 +1,60 @@
 import { useEffect, useState } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 export default function HowTo() {
-  const [isiOS, setIsiOS] = useState(false);
-
-  useEffect(async () => {
-    setIsiOS(iOS());
-  }, []);
-
-  const iOS = () => {
-    return (
-      [
-        "iPad Simulator",
-        "iPhone Simulator",
-        "iPod Simulator",
-        "iPad",
-        "iPhone",
-        "iPod",
-      ].includes(navigator.platform) ||
-      // iPad on iOS 13 detection
-      (navigator.userAgent.includes("Mac") && "ontouchend" in document)
-    );
-  };
   return (
     <Row
       className="align-items-center bubble-background text-center"
       id="how-to"
     >
       <div className="h-100 position-relative p-0 margin-top-100">
-        <h2>Revised kor walkthrough (How to buy a miner on KOR’s DAPP) </h2>
+        <h4>How to buy a miner on KOR’s DAPP</h4>
         <video
-          autoPlay={isiOS ? false : true}
-          controls={isiOS ? true : false}
-          muted
+          autoPlay={false}
+          controls={true}
           loop
           src="video/Revised kor walkthrough.mp4"
-          className="w-100"
+          className="w-80"
         />
       </div>
       <div className="h-100 position-relative p-0 margin-top-100">
-        <h2>USDC vid (How to add USDC to metamask)</h2>
+        <h4>How to add USDC to metamask</h4>
         <video
-          autoPlay={isiOS ? false : true}
-          controls={isiOS ? true : false}
-          muted
+          autoPlay={false}
+          controls={true}
           loop
           src="video/USDC vid.mp4"
-          className="w-100"
+          className="w-80"
         />
       </div>
       <div className="h-100 position-relative p-0 margin-top-100">
-        <h2>How to deposit money to crypto.com</h2>
+        <h4>How to deposit money to crypto.com</h4>
         <video
-          autoPlay={isiOS ? false : true}
-          controls={isiOS ? true : false}
-          muted
+          autoPlay={false}
+          controls={true}
           loop
-          src="https://www.youtube.com/watch?v=3IK555AX8nc"
-          className="w-100"
+          src="video/How to buy Bitcoin.mp4"
+          className="w-80"
         />
       </div>
       <div className="h-100 position-relative p-0 margin-top-100">
-        <h2>How to withdraw and deposit crypto to metamask</h2>
+        <h4>How to withdraw and deposit crypto to metamask</h4>
         <video
-          autoPlay={isiOS ? false : true}
-          controls={isiOS ? true : false}
-          muted
+          autoPlay={false}
+          controls={true}
           loop
-          src="https://www.youtube.com/watch?v=4WG3Q6t4XqE"
-          className="w-100"
+          src="video/How to ADD MONEY to METAMASK.mp4"
+          className="w-80"
         />
       </div>
       <div className="h-100 position-relative p-0 margin-top-100">
-        <h2>How to sell your NFT on Opensea</h2>
+        <h4>How to sell your NFT on Opensea</h4>
         <video
-          autoPlay={isiOS ? false : true}
-          controls={isiOS ? true : false}
-          muted
+          autoPlay={false}
+          controls={true}
           loop
-          src="https://www.youtube.com/watch?v=4ftt5j8hwS4"
-          className="w-100"
+          src="video/How to Sell NFTs on OpenSea.mp4"
+          className="w-80"
         />
       </div>
     </Row>
